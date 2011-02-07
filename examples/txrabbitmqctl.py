4068ffccfb71c23a2ce1b5acc277b7f0b6d725e8
Dorian Raymer <deldotdr@gmail.com>
2011-02-07 14:36:54 -0800

    Clean up example

--- examples/txrabbitmqctl.py
+++ examples/txrabbitmqctl.py
@@ -22,63 +22,199 @@ from txrabbitmq.service import RabbitMQControlService
 
 from anion import messaging
 
-class RabbitMQContro():
+
+##################################################################
+## Client version 1
+## Implement each method from the irabbitmqctl.IRabbitMQControlService
+## interface. 
+
+class RabbitMQControlClientVerbose(messaging.Entity):
     """
-    Functionality of 'rabbitmqctl' exposed as a Twisted Service.
+    This is the verbose implementation of the Control Client. All the
+    irabbitmqctl.IRabbitMQControlService methods are explicitly
+    implemented. Compare this to the alternate implementation below, which
+    generates the methods from the interface definition, automatically.
     """
 
-    def add_user(username, password):
+    def __init__(self, remote_name):
+        self.remote_name = remote_name # The name of the service this is a client for
+
+    def callRemote(self, cmd_dict):
+        """
+        The Commands bound to this client use this.
+        """
+        data = json.dumps(cmd_dict) #XXX handle encoding errors
+        d = self.nchannel.rpc_send(self.remote_name, data)
+
+        def decode_response(data):
+            return json.loads(data)
+        d.addCallback(decode_response)
+        d.addErrback(defer.logError) # the caller handles errors
+        return d
+     
+    def add_user(self, username, password):
         """add new user with given password"""
+        return self.callRemote({
+                'required': (username, password), 
+                'optional': {}, 
+                'name': 'add_user'
+                })
 
-    def delete_user(username):
+    def delete_user(self, username):
         """delete user"""
+        return self.callRemote({
+                'required': (username,), 
+                'optional': {}, 
+                'name': 'delete_user'
+                })
 
-    def change_password(username, password):
+    def change_password(self, username, password):
         """change user password"""
+        return self.callRemote({
+                'required': (username, password), 
+                'optional': {}, 
+                'name': 'change_password'
+                })
 
-    def list_users():
+    def list_users(self):
         """list all users"""
+        return self.callRemote({
+                'required': (), 
+                'optional': {}, 
+                'name': 'list_users'
+                })
 
-    def add_vhost(vhostpath):
+    def add_vhost(self, vhostpath):
         """add new vhost"""
+        return self.callRemote({
+                'required': (vhostpath,), 
+                'optional': {}, 
+                'name': 'add_vhost'
+                })
 
-    def delete_vhost(vhostpath):
+    def delete_vhost(self, vhostpath):
         """delete vhost"""
+        return self.callRemote({
+                'required': (vhostpath,), 
+                'optional': {}, 
+                'name': 'delete_vhost'
+                })
 
-    def list_vhosts():
+    def list_vhosts(self):
         """list all vhosts"""
+        return self.callRemote({
+                'required': (), 
+                'optional': {}, 
+                'name': 'list_vhosts'
+                })
 
-    def set_permissions(username, config_regex, write_regex, read_regex, vhostpath=None):
+    def set_permissions(self, username, config_regex, write_regex, read_regex, vhostpath=None):
         """set permission of a user to broker resources"""
+        return self.callRemote({
+                'required': (username, config_regex, write_regex, read_regex), 
+                'optional': {'vhostpath': vhostpath}, 
+                'name': 'set_permissions'
+                })
 
-    def clear_permissions(username, vhostpath=None): 
+    def clear_permissions(self, username, vhostpath=None): 
         """clear user permissions"""
+        return self.callRemote({
+                'required': (username,), 
+                'optional': {'vhostpath': vhostpath},
+                'name': 'clear_permissions'
+                })
 
-    def list_vhost_permissions(vhostpath=None): 
+    def list_vhost_permissions(self, vhostpath=None): 
         """list all users permissions"""
+        return self.callRemote({
+                'required': (), 
+                'optional': {'vhostpath': vhostpath}, 
+                'name': 'list_vhost_permissions'
+                })
 
-    def list_user_permissions(username=None): 
+    def list_user_permissions(self, username=None): 
         """list all users permissions"""
+        return self.callRemote({
+                'required': (), 
+                'optional': {'username': username}, 
+                'name': 'list_user_permissions'
+                })
 
-    def list_queues(vhostpath=None, queueinfoitem=None):
-        """list all queues"""
+    def list_queues(self, vhostpath=None, queueinfoitem=None):
+        """list all queues
+        @todo need a list of queueinfoitems 
+        """
+        return self.callRemote({
+                'required': (), 
+                'optional': {'vhostpath': vhostpath, 'queueinfoitem': queueinfoitem}, 
+                'name': 'list_queues'
+                })
 
-    def list_exchanges(vhostpath=None, exchangeinfoitem=None):
-        """list all exchanges"""
+    def list_exchanges(self, vhostpath=None, exchangeinfoitem=None):
+        """list all exchanges
+        @todo need a list of exchangeinfoitems 
+        """
+        return self.callRemote({
+                'required': (), 
+                'optional': {'vhostpath': vhostpath, 'exchangeinfoitem': exchangeinfoitem}, 
+                'name': 'list_exchanges'
+                })
 
-    def list_bindings(vhostpath=None):
+    def list_bindings(self, vhostpath=None):
         """list all bindings"""
+        return self.callRemote({
+                'required': (), 
+                'optional': {'vhostpath': vhostpath}, 
+                'name': 'list_bindings'
+                })
 
-    def list_connections(connectioninfoitem=None):
-        """list all connections"""
+    def list_connections(self, connectioninfoitem=None):
+        """list all connections
+        @todo need a list of connectioninfoitems
+        """
+        return self.callRemote({
+                'required': (), 
+                'optional': {'connectioninfoitem': connectioninfoitem},
+                'name': 'list_connections'
+                })
 
 
-#########################################################################
-## An encoding for the RabbitMQControlService commands
-## This is essentially a little protocol. 
-## The irabbitmqctl.IRabbitMQControlService interface specifies the names
-## and call signatures for every rabbitmqctl method.
-##
+##############################################################################
+## Client version 2
+## Generate commands from the interface irabbitmqctl.IRabbitMQControlService
+
+class RabbitMQControlClientFromInterface(messaging.Entity):
+    """
+    Provides IRabbitMQControlService interface.
+
+    In this version of the client, the methods are automatically generated
+    from the interface. It's harder to see how the client works (you have
+    to refer to the interface), but you gain robustness by directly using
+    the interface.
+    """
+
+    def __init__(self, service_name):
+        self.service_name = service_name # The name of the service this is a client for
+        namesAndDesc = irabbitmqctl.IRabbitMQControlService.namesAndDescriptions()
+        for name, command in namesAndDesc:
+            info = command.getSignatureInfo()
+            doc = command.getDoc()
+            setattr(self, name, Command(self, name, info, doc))
+
+    def callRemote(self, cmd_dict):
+        """
+        Send the command to the remote service.
+        Returns a Deferred that fires with the response.
+        """
+        data = json.dumps(cmd_dict) #XXX handle encoding errors
+        d = self.nchannel.rpc_send(self.service_name, data)
+
+        def decode_response(data):
+            return json.loads(data)
+        d.addCallback(decode_response)
+        d.addErrback(defer.logError) # the caller handles errors
+        return d
+ 
 class Command(object):
     """
     Command method generated from interface.
@@ -94,49 +230,25 @@ class Command(object):
         self.__doc__ = doc
 
     def __call__(self, *args, **kwargs):
+        command_dict = self._commad_dict_from_call(*args, **kwargs)
+        return self.client.callRemote(command_dict)
+
+    def _commad_dict_from_call(self, *args, **kwargs):
         if not len(args) == len(self.required):
             raise TypeError('%s() takes at least %d arguments (%d given)' %
                     (self.name, len(self.required), len(args)))
-        cmd_msg = {}
-        cmd_msg['required'] = args
-        cmd_msg['optional'] = {}
+        cmd_dict = {}
+        cmd_dict['name'] = self.name
+        cmd_dict['required'] = args
+        cmd_dict['optional'] = {}
         for k, v in self.optional.iteritems():
-            cmd_msg['optional'][k] = kwargs.get(k, v)
-        if not (len(cmd_msg['required']) + len(cmd_msg['optional'])) == len(self.positional):
+            cmd_dict['optional'][k] = kwargs.get(k, v)
+        if not (len(cmd_dict['required']) + len(cmd_dict['optional'])) == len(self.positional):
             raise TypeError('%s() takes %d arguments (%d given)' %
-                    (self.name, len(self.positional), len(cmd_msg)))
-        cmd_msg['name'] = self.name
-        return self.client.callRemote(cmd_msg)
+                    (self.name, len(self.positional), len(cmd_dict)))
+        return cmd_dict
+ 
 
-
-##############################################################################
-## Client
-
-class RabbitMQControlClient(messaging.Entity):
-    """
-    Provides IRabbitMQControlService interface.
-
-    In this version of the client, the methods are automatically generated
-    from the interface. It's harder to see how the client works (you have
-    to refer to the interface), but you gain robustness by directly using
-    the interface.
-    """
-
-    def __init__(self, remote_name):
-        self.remote_name = remote_name # The name of the service this is a client for
-        namesAndDesc = irabbitmqctl.IRabbitMQControlService.namesAndDescriptions()
-        for name, command in namesAndDesc:
-            info = command.getSignatureInfo()
-            doc = command.getDoc()
-            setattr(self, name, Command(self, name, info, doc))
-
-    def callRemote(self, cmd_msg):
-        """
-        The Commands bound to this client use this.
-        """
-        data = json.dumps(cmd_msg) #XXX handle encoding errors
-        return self.nchannel.rpc_send(self.remote_name, data)
-     
 
 ################################################################################
 ## Service
@@ -172,6 +284,10 @@ class RabbitMQControlEntityFromService(messaging.Entity):
 
 
 def make_rabbitmqctl_service():
+    """
+    Boiler plate construction of txrabbitmq Service.
+    Usually Twisted Service libraries provide a makeService method.
+    """
     from twotp import Process, readCookie, buildNodeName
     cookie = readCookie()
     nodeName = buildNodeName('txrabbitmq')
@@ -179,26 +295,28 @@ def make_rabbitmqctl_service():
     service = RabbitMQControlService(process, 'rabbit')
     return service
 
+###############################################################################
+## Example usage
 
-def test_node():
-    node = messaging.NodeManager()
-    node_container = messaging.NodeContainer(node)
+def test_service_and_client():
+    # Start the Node
+    node_manager = messaging.NodeManager()
+    node_container = messaging.NodeContainer(node_manager)
     reactor.connectTCP('localhost', 5672, node_container)
 
-    rserv = make_rabbitmqctl_service()
-    rent = RabbitMQControlEntityFromService(rserv)
-    node.addEntity('rabbitmqctl', rent, messaging.RPCChannel)
-    cl = RabbitMQControlClient('rabbitmqctl')
-    node.addEntity('cl', cl, messaging.NChannel)
-    return node, cl
+    # Configure the service Entity, and add it to the node
+    rservice = make_rabbitmqctl_service()
+    rentity = RabbitMQControlEntityFromService(rservice)
+    node_manager.addEntity('rabbitmqctl', rentity, messaging.RPCChannel)
+
+    # Configure the client Entity, and add it to the node
+    client = RabbitMQControlClientFromInterface('rabbitmqctl')
+    node_manager.addEntity('anonymous', client, messaging.NChannel)
+    return node_manager, client
 
 
 if __name__ == '__main__':
-    test_node()
+    test_service_and_client()
     reactor.run()
-
-
-
-
 
 
